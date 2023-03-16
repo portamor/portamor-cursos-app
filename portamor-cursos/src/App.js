ba72cd394e90191848c5bd9fad33dc5f0c717049
DiameCjs <diame.cjs@gmail.com>
2023-03-16 18:26:45 -0500

    create react app

---
+++ portamor-cursos/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route />
+          <Route />
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
