467675755f1f13976e81d0a670266a0016d58855
DiameCjs <diame.cjs@gmail.com>
2023-03-17 14:42:04 -0500

    diseño navbar responsive

--- Portamor-Front/src/App.js
+++ Portamor-Front/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route />
+          <Route path="/" element={<NavBar />} />
           <Route />
           <Route />
         </Routes>
