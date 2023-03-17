c62726be3c49aa7a2d5474932f9e8c130d49919f
DiameCjs <diame.cjs@gmail.com>
2023-03-17 15:14:22 -0500

    config store, reducer

---
+++ Portamor-Front/src/Redux/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+   
+  };
+  
+  function rootReducer(state = initialState, action) {
+    switch (action.type) {
+  
+     
+  
+        default:
+          return state;
+    }
+  }
+  
+  export default rootReducer;
