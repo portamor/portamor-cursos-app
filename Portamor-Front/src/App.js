c5cfd9687e09866edd2433116a98757db7bdc50b
DiameCjs <diame.cjs@gmail.com>
2023-03-21 21:45:36 -0500

    navFilter

--- Portamor-Front/src/App.js
+++ Portamor-Front/src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
 import CourseDetail from "./Components/CourseDetail/CourseDetail";
+import NavFilter from "./Components/NavFilter/NavFilter";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <NavBar/>
+        <NavBar />
+        <NavFilter />
         <Routes>
           <Route />
-          <Route exact path="/detalle-curso/:id" Component={CourseDetail}/>
+          <Route exact path="/detalle-curso/:id" Component={CourseDetail} />
           <Route />
         </Routes>
       </BrowserRouter>
