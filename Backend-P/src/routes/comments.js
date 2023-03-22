9844bd88832df0c9f5e8e053929c0190185b8372
Seba <sebastian.pitra10@gmail.com>
2023-03-22 18:05:09 -0300

    feat: routes to comments

--- Backend-P/src/routes/comments.js
+++ Backend-P/src/routes/comments.js
@@ -4,12 +4,12 @@ const commentRouter     = Router()
 
 // ---- POST
 commentRouter.post('/:courseId', commentController.postComment);
-
 //----- GET
 commentRouter.get('/:courseId',  commentController.getAllCommentsByCourseId)
-
 //----- PUT
-commentRouter.put('/:courseId',  commentController.putComment)
-
+commentRouter.put('/:commentId', commentController.putComment)
+commentRouter.put('/restore/:commentId', commentController.restoreComment)
+//----- DELETE
+commentRouter.delete('/:commentId', commentController.deleteComment)
 
 module.exports = commentRouter;
