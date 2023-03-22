11465ac8d525b746e7309c17421dfa4b26066dd0
Seba <sebastian.pitra10@gmail.com>
2023-03-22 17:19:14 -0300

    feat: put controller method was created

--- Backend-P/src/controllers/commentController.js
+++ Backend-P/src/controllers/commentController.js
@@ -4,17 +4,17 @@ const courseService         = require('../services/courseService')
 
 const postComment = async (req, res) => {
   try {
-    const { id } = req.params;
+    const { courseId } = req.params;
     const { content, satisfaction } = req.body;
 
     if(!content || !satisfaction) {
       throw new Error("Estan faltando valores para crear un comentario")
     }
 
-    const courseFound = await courseService.getCourseById(id)
+    const courseFound = await courseService.getCourseById(courseId)
 
     if(!courseFound) {
-      throw new Error(`No se ha encontrado ningun curso con este ID: ${id}`)
+      throw new Error(`No se ha encontrado ningun curso con este ID: ${courseId}`)
     }
 
     const createdComment = await commentService.createCommentInDatabase({
@@ -35,45 +35,49 @@ const postComment = async (req, res) => {
 
 const getAllCommentsByCourseId = async (req, res) => {
   try {
-    const { id } = req.params;
+    const { courseId } = req.params;
     
-    if(!id) throw new Error("No se ha enviado un id de un curso para buscar comentarios")
+    if(!courseId) throw new Error("No se ha enviado un id de un curso para buscar comentarios")
 
-    const courseFound = await courseService.getCourseById(id)
+    const courseFound = await courseService.getCourseById(courseId)
 
     if(!courseFound) {
-      throw new Error(`No se ha encontrado ningun curso con este ID: ${id}`)
+      throw new Error(`No se ha encontrado ningun curso con este ID: ${courseId}`)
     }
 
-    const { Comments } = await commentService.getCommentsByCourse(courseFound.id)
+    const { Comments } = await commentService.getCommentsByCourseId(courseFound.id)
 
-    res.status(200).json({msg: "Comentarios encontrados exitosamente", data: Comments})
+    res.status(200).json({msg: "Comentarios encontrados con exito", data: Comments})
   } catch (error) {
     res.status(400).json({msg: error.message})
   }
 }
 
-// const putComment = async (req, res) => {
-//   try {
-//     const { id } = req.params;
+const putComment = async (req, res) => {
+  try {
+    const { commentId } = req.params;
     
-//     if(!id) throw new Error("No se ha enviado un id para actualizar un comentario")
+    if(!commentId) throw new Error("No se ha enviado un id para actualizar un comentario")
 
-//     const courseFound = await courseService.getCourseById(id)
+    const foundComment = await commentService.getCommentById(commentId)
 
-//     if(!courseFound) {
-//       throw new Error(`No se ha encontrado ningun curso con este ID: ${id}`)
-//     }
+    if(!foundComment) {
+      throw new Error(`No se ha encontrado ningun comentario con el ID: ${commentId}`)
+    }
 
-//     const { Comments } = await commentService.getCommentsByCourse(courseFound.id)
-
-//     res.status(200).json({msg: "Curso encontrado exitosamente", data: Comments})
-//   } catch (error) {
-//     res.status(400).json({msg: error.message})
-//   }
-// }
+    const updatedComment = await commentService.updateComment({
+      id: commentId,
+      data: req.body
+    })
+    
+    res.status(200).json({msg: "Comentario actualizado con exito", data: updatedComment})
+  } catch (error) {
+    res.status(400).json({msg: error.message})
+  }
+}
 
 module.exports = {
   postComment,
   getAllCommentsByCourseId,
+  putComment
 }
