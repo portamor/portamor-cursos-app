0036133fa5787185012b883ebfb8c89a591ae1bf
Seba <sebastian.pitra10@gmail.com>
2023-03-23 14:33:22 -0300

    fix: sintax was changed

--- Backend-P/src/models/Instructor.js
+++ Backend-P/src/models/Instructor.js
@@ -1,31 +1,32 @@
-const {DataTypes, Sequelize} = require('sequelize');
+const {DataTypes, Sequelize} = require("sequelize");
 
 module.exports = sequelize => {
-    sequelize.define('Instructor',
+  sequelize.define(
+    "Instructor",
     {
       id: {
         type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-            allowNull: false
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
       },
       instructorName: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
-      instructorScore:{
+      instructorScore: {
         type: DataTypes.STRING,
-        allowNull: true
+        allowNull: true,
       },
       instructorReview: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
-      instructorDescription:{
+      instructorDescription: {
         type: DataTypes.STRING,
-        allowNull: true
-      }
+        allowNull: true,
+      },
     },
-    { timestamps: false }
-    )
-}
+    {timestamps: false}
+  );
+};
