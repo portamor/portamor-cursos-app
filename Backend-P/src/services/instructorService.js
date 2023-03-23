3959fae6403f8a8a581de954f8bdb160917cf7d7
Seba <sebastian.pitra10@gmail.com>
2023-03-23 14:32:23 -0300

    feat: createCommentInDb service was created

--- Backend-P/src/services/instructorService.js
+++ Backend-P/src/services/instructorService.js
@@ -1,5 +1,17 @@
 const { Instructor } = require("../database")
 
+const createIntructorInDB = async ({ courseId, data }) => {
+  const createdInstructor = await Instructor.create({
+    instructorName: data.instructorName,
+    instructorScore: data.instructorScore,
+    instructorReview: data.instructorReview,
+    instructorDescription: data.instructorDescription,
+    CourseId: courseId
+  });
+
+  return createdInstructor;
+}
+
 module.exports = {
-  
+  createIntructorInDB,
 }
