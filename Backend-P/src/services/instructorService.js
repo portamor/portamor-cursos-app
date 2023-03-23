58a8d298602c9d5a45da5d06dbdcbf0cd440ea9f
Seba <sebastian.pitra10@gmail.com>
2023-03-23 14:50:06 -0300

    feat: getAllInstructors service was created

--- Backend-P/src/services/instructorService.js
+++ Backend-P/src/services/instructorService.js
@@ -2,16 +2,22 @@ const { Instructor } = require("../database")
 
 const createIntructorInDB = async ({ courseId, data }) => {
   const createdInstructor = await Instructor.create({
-    instructorName: data.instructorName,
-    instructorScore: data.instructorScore,
-    instructorReview: data.instructorReview,
-    instructorDescription: data.instructorDescription,
+    description: data.description,
+    name: data.name,
+    profile_picture: data.profile_picture,
+    score: data.score,
+    review: data.review,
     CourseId: courseId
   });
 
   return createdInstructor;
 }
 
+const getAllIntructorFromDB = async () => {
+  return Instructor.findAll();
+}
+
 module.exports = {
   createIntructorInDB,
+  getAllIntructorFromDB
 }
