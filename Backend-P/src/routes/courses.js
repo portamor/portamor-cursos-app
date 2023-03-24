b66f98bf937ab553de2360152f09eda00fd9184f
Lizhana <lizhanita@gmail>
2023-03-24 00:15:21 -0300

    courseServer, get, post, put, delete and restore

--- Backend-P/src/routes/courses.js
+++ Backend-P/src/routes/courses.js
@@ -1,13 +1,28 @@
-const {Router}          = require('express');
-const coursesController = require('../controllers/coursesController.js');
+const { Router } = require("express");
+const coursesController = require("../controllers/coursesController.js");
 
-const router = Router()
+const router = Router();
 
 // ---- POST
-router.post('/', coursesController.postCourse);
+router.post("/", coursesController.postCourse);
 
 // ---- GET
-router.get('/',  coursesController.getCourses);
+router.get("/", coursesController.getCourses);
 
+// ---- GET BY TITLE
+router.get("/title/:title", coursesController.getCourseTitle);
+
+// ---- GET BY TYPE
+router.get("/type/:typeCourse", coursesController.getCourseType);
+
+// ---- GET BY GENRE
+router.get("/genre/:genreCourse", coursesController.getCourseGenre);
+
+// ---- PUT
+router.put("/", coursesController.putCourse);
+router.put("/restore/:id", coursesController.restoreCouse);
+
+// ----DELETE
+router.delete("/", coursesController.deleteACourse);
 
 module.exports = router;
