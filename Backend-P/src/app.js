19e3129aa9712e1ca3928d2df94e5def8bd9712b
Seba <sebastian.pitra10@gmail.com>
2023-03-27 14:13:52 -0300

    fix: sintax was changed

--- Backend-P/src/app.js
+++ Backend-P/src/app.js
@@ -1,19 +1,21 @@
-require("dotenv").config();
-const express = require('express');
+const express      = require('express');
 const cookieParser = require('cookie-parser');
-const morgan = require('morgan')
-const session = require('express-session');
-const cors = require('cors');
- const router = require('./index.js')
+const morgan       = require('morgan')
+const session      = require('express-session');
+const cors         = require('cors');
+const router       = require('./routes/index.js')
+require("dotenv").config();
 require('./database.js')
-
-
 
 const server = express()
 
 server.use(cors())
-
+server.use(express.text());
+server.use(cookieParser());
+server.use(morgan("dev"));
 server.use(express.urlencoded({ extended: false }));
+server.use("/", router);
+
 server.use((req, res, next) => {
   if (req.originalUrl === "/webhook") {
     next();
@@ -21,31 +23,26 @@ server.use((req, res, next) => {
     express.json({ limit: "50mb" })(req, res, next);
   }
 });
-server.use(express.text());
-server.use(cookieParser());
 
-server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, ContentType, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-  });
+  "Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, ContentType, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
   
-  server.use("/", router);
-
-  // Error catching endware.
+// Error catching endware.
 server.use((err, req, res, next) => {
-    // eslint-disable-line no-unused-vars
-    const status = err.status || 500;
-    const message = err.message || err;
-    console.error(err,'aqio');
-    res.status(status).send(message);
-  });
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err,'aqio');
+  res.status(status).send(message);
+});
 
 
 module.exports = server
