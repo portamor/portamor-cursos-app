4bc22a3e44b9f57cbb443cd01f8d08e1931a33b9
Seba <sebastian.pitra10@gmail.com>
2023-03-29 16:05:33 -0300

    feat: CustomButton was created

---
+++ client/src/Components/CustomButton/CustomButton.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./CustomButton.module.css";
+
+const CustomButton = ({content, primary, type}) => {
+  const customCssClassName = primary === true ? "important" : "secondary";
+  
+  return (
+    <button 
+    type={type}
+    className={styles["custom-button-main-" + customCssClassName]}>
+      {content}
+    </button>
+  )
+};
+
+export default CustomButton;
