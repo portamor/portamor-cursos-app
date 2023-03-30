4507929f462a4c80df3d85585099116b9f14ddbb
Seba <sebastian.pitra10@gmail.com>
2023-03-30 09:58:15 -0300

    feat:section path added to index router

--- Backend-P/src/routes/index.js
+++ Backend-P/src/routes/index.js
@@ -7,11 +7,13 @@ const coursesRouter    = require('./courses')
 const videosRouter     = require('./videos')
 const instructorRouter = require('./instructorRouter')
 const reviewRouter     = require('./reviewRouter')
+const sectionRouter    = require('./routes/sectionsRouter')
 
 router.use('/users',      usersRouter);
 router.use('/courses',    coursesRouter);
 router.use('/videos',     videosRouter);
 router.use('/instructor', instructorRouter)
 router.use('/review',     reviewRouter)
+router.use('/section',    sectionRouter)
 
 module.exports = router
