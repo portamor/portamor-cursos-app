035e54719d203168cddec4f9cf136930764e88c9
Seba <sebastian.pitra10@gmail.com>
2023-03-30 16:21:16 -0300

    fix(courseRouter): sintax was changed

--- Backend-P/src/routes/coursesRouter.js
+++ Backend-P/src/routes/coursesRouter.js
@@ -7,23 +7,15 @@ const router = Router();
 router.post("/", coursesController.postCourse);
 
 // ---- GET
-router.get("/", coursesController.getCourses);
-
-// ---- GET BY ID -- INCLUDE VIDEOS
-router.get("/id/:id", coursesController.getCourseById)
-
-// ---- GET BY TITLE
-router.get("/title/:title", coursesController.getCourseTitle);
-
-// ---- GET BY TYPE
-router.get("/type/:typeCourse", coursesController.getCourseType);
-
-// ---- GET BY GENRE
-router.get("/genre/:genreCourse", coursesController.getCourseGenre);
+router.get("/",             coursesController.getAllCourses);
+router.get("/id/:id",       coursesController.getCourseById)
+router.get("/title/:title", coursesController.getCourseByTitle);
+router.get("/type/:type",   coursesController.getCourseByType);
+router.get("/genre/:genre", coursesController.getCourseByGenre);
 
 // ---- PUT
-router.put("/:id", coursesController.putCourse);
-router.put("/restore/:id", coursesController.restoreCouse);
+router.put("/:id",         coursesController.putCourse);
+router.put("/restore/:id", coursesController.restoreCourse);
 
 // ----DELETE
 router.delete("/:id", coursesController.deleteACourse);
