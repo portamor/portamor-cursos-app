b451edc23d28365f2a2bde7579add3bb601b9963
Seba <sebastian.pitra10@gmail.com>
2023-03-31 20:51:21 -0300

    feat: constants folder was created

---
+++ client/src/constants/index.js
@@ -0,0 +1,5 @@
+// To ClassDetail
+export const COMENTARIOS          = "COMENTARIOS"; 
+export const MATERIALES           = "MATERIALES";
+export const PARTICIPANTES        = "PARTICIPANTES"; 
+export const PREGUNTAS_FRECUENTES = "PREGUNTAS FRECUENTES"; 
