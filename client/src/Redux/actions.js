4ed13b41b65386cf1e05d301ff9d4d138cfa6cfe
Seba <sebastian.pitra10@gmail.com>
2023-03-31 11:01:15 -0300

    feat: action to get course detail

--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -1,23 +1,35 @@
-import axios from "axios"
+import axios from "axios";
+export const GET_COURSE_DETAIL = "GET_COURSE_DETAIL";
 
 export function getCourses() {
-    return async function (dispatch) {
-      try {
-        var json = await axios.get("http://localhost:3001/courses");
-        return dispatch({
-          type: "GET_COURSES",
-          payload: json.data,
-        });
-      } catch (error) {
-        console.log("Error en getCourses/actions", error);
-      }
-    };
-  }
-
-  export function postUser (payload){
-    return async function (dispatch){
-        const response = await axios.post('http://localhost:3001/users', payload);
-        return response;
+  return async function (dispatch) {
+    try {
+      var json = await axios.get("http://localhost:3001/courses");
+      return dispatch({
+        type: "GET_COURSES",
+        payload: json.data,
+      });
+    } catch (error) {
+      console.log("Error en getCourses/actions", error);
     }
-  }
-  
+  };
+}
+
+export function getCourseDetail(id) {
+  return async function (dispatch) {
+    try {
+      var courseDetail = await axios.get(`http://localhost:3001/courses/id/${id}`);
+
+      return dispatch({ type: GET_COURSE_DETAIL, payload: courseDetail.data.data });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+}
+
+export function postUser(payload) {
+  return async function (dispatch) {
+    const response = await axios.post("http://localhost:3001/users", payload);
+    return response;
+  };
+}
