11d70d79f1df46d7e4f3c4315ecd2061922ae2b9
Seba <sebastian.pitra10@gmail.com>
2023-03-31 22:31:30 -0300

    feat(constants): new constants for buttons in ClassDetail component

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -3,3 +3,5 @@ export const COMENTARIOS          = "COMENTARIOS";
 export const MATERIALES           = "MATERIALES";
 export const PARTICIPANTES        = "PARTICIPANTES"; 
 export const PREGUNTAS_FRECUENTES = "PREGUNTAS FRECUENTES"; 
+export const VER_TEMARIO          = "VER TEMARIO"; 
+export const SOBRE_EL_INSTRUCTOR  = "SOBRE_EL_INSTRUCTOR"; 
