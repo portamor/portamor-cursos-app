1e3008ee530af2c616693d0068f10118d65b2e1d
Seba <sebastian.pitra10@gmail.com>
2023-04-03 13:27:34 -0300

    fix: state properties location was changed from ClassDetail from SelectedData

--- client/src/Components/SelectedData/SelectedData.jsx
+++ client/src/Components/SelectedData/SelectedData.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 //----Components
 import ReviewCard from "../ReviewCard/ReviewCard";
 import UserCard   from "../UserCard/UserCard";
@@ -15,23 +14,17 @@ import { useDispatch, useSelector } from "react-redux";
 const SelectedData = ({courseDetail, courseId, selectedButtonContent}) => {
   const dispatch = useDispatch();
   
+  const courseUsers    = useSelector((state) => state.courseUsers);
+  const courseReviews  = useSelector((state) => state.courseReviews);
   const courseSections = useSelector((state) => state.courseSections);
-  const courseReviews = useSelector((state) => state.courseReviews);
-  const courseUsers = useSelector((state) => state.courseUsers);
-  const courseInstructor = useSelector((state) => state.courseInstructor);
 
-
-   switch (selectedButtonContent) {
+  switch (selectedButtonContent) {
     case constants.VER_TEMARIO:
       if(!courseSections.length) dispatch(actions.getSectionsByCourseId(courseId)) 
 
       return <CourseAccordion sections={courseSections}/>
     
     case constants.SOBRE_EL_INSTRUCTOR:
-      // if(!courseInstructor) {
-      //   dispatch(actions.getInstructorById(courseDetail.InstructorId));   
-      // }
-
       return <InstructorDetail instructorId={courseDetail.InstructorId}/>;
 
     case constants.COMENTARIOS:
