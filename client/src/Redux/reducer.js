f7ac24b51fb1776fed84f651bd02be6a62a66e52
Seba <sebastian.pitra10@gmail.com>
2023-04-03 15:28:24 -0300

    feat(reducer): videoDetail property was added in initialState

--- client/src/Redux/reducer.js
+++ client/src/Redux/reducer.js
@@ -1,10 +1,4 @@
-import {
-  GET_COURSE_DETAIL,
-  GET_INSTRUCTOR_BY_ID, 
-  GET_REVIEWS_BY_COURSE_ID, 
-  GET_SECTIONS_BY_COURSE_ID, 
-  GET_USERS_BY_COURSE_ID
-} from "../constants";
+import * as actions from "../constants";
 
 const initialState = {
   courses: [],
@@ -13,6 +7,7 @@ const initialState = {
   courseSections: [],
   courseUsers: [],
   courseReviews: [],
+  videoDetail: {},
   user: [],
 };
 
@@ -24,34 +19,40 @@ function rootReducer(state = initialState, action) {
         courses: action.payload,
       };
 
-    case GET_COURSE_DETAIL:
+    case actions.GET_COURSE_DETAIL:
       return {
         ...state,
         courseDetail: action.payload,
       };
       
-    case GET_SECTIONS_BY_COURSE_ID:
+    case actions.GET_SECTIONS_BY_COURSE_ID:
       return {
         ...state,
         courseSections: action.payload,
       };
       
-    case GET_REVIEWS_BY_COURSE_ID:
+    case actions.GET_REVIEWS_BY_COURSE_ID:
       return {
         ...state,
         courseReviews: action.payload,
       };
       
-    case GET_USERS_BY_COURSE_ID:
+    case actions.GET_USERS_BY_COURSE_ID:
       return {
         ...state,
         courseUsers: action.payload,
       };
 
-    case GET_INSTRUCTOR_BY_ID:
+    case actions.GET_INSTRUCTOR_BY_ID:
       return {
         ...state,
         courseInstructor: action.payload,
+      };
+    
+    case actions.GET_VIDEO_BY_ID:
+      return {
+        ...state,
+        videoDetail: action.payload,
       };
 
     default:
