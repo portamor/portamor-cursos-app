4eefc81e92a85763d59063b78d0901c6aa0a58a3
DiameCjs <diame.cjs@gmail.com>
2023-04-04 09:04:51 -0500

    package

--- Backend-P/index.js
+++ Backend-P/index.js
@@ -3,7 +3,7 @@ const server   = require('./src/app');
 
 require('dotenv').config();
 
-conn.sync({ force: false }).then(()=> {
+conn.sync({ force: true}).then(()=> {
   server.listen(process.env.PORT, () => {
     console.log( "\n" + "%s listening at " + process.env.PORT + "\n" + Date() + "\n" )
   });
