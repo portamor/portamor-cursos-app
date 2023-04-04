49378aa3ecbb32288ba2581637c1da67158299c4
Seba <sebastian.pitra10@gmail.com>
2023-04-04 15:19:19 -0300

    fix(constants): module folders were created

---
+++ client/src/constants/createReviewConstants/index.js
@@ -0,0 +1,5 @@
+export const EXCELENTE   = "Excelente";
+export const RECOMENDADO = "Recomendado";
+export const BUENO       = "Bueno";
+export const NO_ME_GUSTO = "No me gusto";
+export const MALO        = "Malo";
