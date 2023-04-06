fde893358d8131d415aa7e4d8a9105dc6b8f2c49
Seba <sebastian.pitra10@gmail.com>
2023-04-06 13:44:14 -0300

    feat(constants): INSTRUCTOR constant was added to validate function

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,4 +1,5 @@
 //to validate
-export const REVIEW = "REVIEW";
-export const VIDEO  = "VIDEO";
+export const REVIEW      = "REVIEW";
+export const VIDEO       = "VIDEO";
+export const INSTRUCTOR  = "INSTRUCTOR";
 
