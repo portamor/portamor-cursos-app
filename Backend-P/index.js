29710568d00e05dd68e10ab73ee4c961b3ddae7d
Lizhana Campos <111819140+Lizhana@users.noreply.github.com>
2023-04-06 16:40:35 -0400

    implementacion de socket.io del lado del back

--- Backend-P/index.js
+++ Backend-P/index.js
@@ -1,10 +1,10 @@
 const { conn } = require('./src/database.js')
-const server   = require('./src/app');
+const serverApp   = require('./src/app');
 
 require('dotenv').config();
 
 conn.sync({ force: false }).then(()=> {
-  server.listen(process.env.PORT, () => {
+  serverApp.listen(process.env.PORT, () => {
     console.log( "\n" + "%s listening at " + process.env.PORT + "\n" + Date() + "\n" )
   });
 });
