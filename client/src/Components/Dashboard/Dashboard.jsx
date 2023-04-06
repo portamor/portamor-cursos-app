621885b412296f3be1753611fe304ee32e656af6
Seba <sebastian.pitra10@gmail.com>
2023-04-06 17:38:08 -0300

    fix: instructor will be step number 2

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -15,6 +15,13 @@ const Dashboard = () => {
   // 3) La creacion de la seciton modificar el estado gobal 'createdSection'
   // 4) Con esta ultima, podes acceder a su id para crear un video
 
+  // Que sucede cuando se quiere crear mas de una seccion?
+  // En este caso, tiene que haber una seccion para elegir en cual seccion se creara el video
+  // Como lo puedo hacer?
+  // The global state will have a property and his content will be an array of createdSections
+  // I can map those sections and select one to create a video in its.
+
+
   // Logica para mostrar los forms
   // 1) Usar la funcion para setear el estado local del componente
   // 2) Pasar esta funcion por props a todos los formularios
@@ -26,12 +33,12 @@ const Dashboard = () => {
   return (
     <div className={styles.div_dashboard} >
       { actualForm === constants.SELECT_COURSE_FORM     && <CreateCourse     setActualForm={setActualForm} /> }
-      { actualForm === constants.SELECT_SECTION_FORM    && <CreateSection    setActualForm={setActualForm}/> }
-      { actualForm === constants.SELECT_VIDEO_FORM      && <CreateVideo      setActualForm={setActualForm} /> }
       { actualForm === constants.SELECT_INSTRUCTOR_FORM && <CreateInstructor setActualForm={setActualForm} /> }
+      {/* Faltan estos dos */}
+      { actualForm === constants.SELECT_SECTION_FORM    && <CreateSection    setActualForm={setActualForm} /> }
+      { actualForm === constants.SELECT_VIDEO_FORM      && <CreateVideo      setActualForm={setActualForm} /> }
     </div>
   )
-
 }
 
 export default Dashboard;
