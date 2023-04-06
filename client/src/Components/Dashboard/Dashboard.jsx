5d49989e1282f82ec7ed6cee8684406dd892bfb3
Seba <sebastian.pitra10@gmail.com>
2023-04-06 17:03:34 -0300

    feat(dashboard): import forms

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -1,29 +1,36 @@
-import React, { useState } from "react";
-import FormCourse from "./FormCourse";
-import FormSectionCreate from "./FormSection";
-import styles from './formCourse.module.css'
+import React, { useState }  from "react";
+import { CreateCourse }     from "../Forms";
+import { CreateInstructor } from "../Forms";
+import { CreateSection }    from "../Forms";
+import { CreateVideo }      from "../Forms";
+import * as constants       from "../../constants";
+
+import styles from "../Forms/CreateCourse/formCourse.module.css"
+
 
 const Dashboard = () => {
+  // Logica para crear los datos
+  // 1) El 1er form es CreateCourse. Este modificara el estago global en 'createdCourse'
+  // 2) Usare ese 'createdCourse' para obtener el courseId para crear la section
+  // 3) La creacion de la seciton modificar el estado gobal 'createdSection'
+  // 4) Con esta ultima, podes acceder a su id para crear un video
 
-    const [formularioActual, setFormularioActual] = useState(null);
+  // Logica para mostrar los forms
+  // 1) Usar la funcion para setear el estado local del componente
+  // 2) Pasar esta funcion por props a todos los formularios
+  // 3) El estado usara los valores: "course", "section", "video", "instructor"
+  // 4) Dependiendo que indica el estado, se mostrara el form para crear un curso, section o los otros
 
+  const [actualForm, setActualForm] = useState(constants.SELECT_COURSE_FORM);
 
-    return (
-        <div className={styles.div_dashboard} >
-            <div>
-            <button className={styles.button_d} onClick={() => setFormularioActual("A")} >Crear Curso</button>
-            <button className={styles.button_d} onClick={() => setFormularioActual("B")} >Crear Seccion</button>
-            <button className={styles.button_d} onClick={() => setFormularioActual("C")} >Añadir Video</button>
-        </div>
-
-                {formularioActual === 'A' && <FormCourse /> }
-                {formularioActual=== 'B' && <FormSectionCreate />}
-
-
-            
-            
-        </div>
-    )
+  return (
+    <div className={styles.div_dashboard} >
+      { actualForm === constants.SELECT_COURSE_FORM     && <CreateCourse     setActualForm={setActualForm} /> }
+      { actualForm === constants.SELECT_SECTION_FORM    && <CreateSection    setActualForm={setActualForm}/> }
+      { actualForm === constants.SELECT_VIDEO_FORM      && <CreateVideo      setActualForm={setActualForm} /> }
+      { actualForm === constants.SELECT_INSTRUCTOR_FORM && <CreateInstructor setActualForm={setActualForm} /> }
+    </div>
+  )
 
 }
 
