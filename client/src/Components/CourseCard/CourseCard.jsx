a66693bbf2072f077aa8896a7d0be36266cd8869
Seba <sebastian.pitra10@gmail.com>
2023-04-11 11:47:50 -0300

    styles: responsive course card was fixed

--- client/src/Components/CourseCard/CourseCard.jsx
+++ client/src/Components/CourseCard/CourseCard.jsx
@@ -6,7 +6,7 @@ const CourseCard = ({ id, title, image }) => {
   return (
     <Link to={`/detalle-curso/${id}`} className={Styles["button-link"]}>
       <div className={Styles["course-card"]}>
-        <img src={image} alt="course-card" height={200} width={399} />
+        <img src={image} alt="course-card" />
         <h3>{title}</h3>
         <button className={Styles["card-button"]}>Ver Más</button>
       </div>
