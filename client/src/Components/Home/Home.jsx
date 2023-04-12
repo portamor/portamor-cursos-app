4013da606e479e62233efcf45c393736708a69b8
Seba <sebastian.pitra10@gmail.com>
2023-04-12 16:17:26 -0300

    fix: prop actualPage to dispatch a action to get all courses

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,11 +1,11 @@
+import Paginated from "../Paginated/Paginated";
 import React     from "react";
 import Styles    from "./Home.module.css";
-import Paginated from "../Paginated/Paginated";
 
 const Home = () => {
   return (
     <div className={Styles["home-container"]}>
-      <Paginated />
+      <Paginated actualPage={"HOME"}/>
     </div>
   );
 };
--- client/src/Components/MyCourses/MyCourses.jsx
+++ client/src/Components/MyCourses/MyCourses.jsx
@@ -1,33 +1,22 @@
 import * as actions    from "../../Redux/actions"
-import CourseCard      from "../CourseCard/CourseCard";
 import Paginated       from "../Paginated/Paginated";
 import React           from "react";
 import styles          from "./MyCourses.module.css";
 import { useEffect }   from "react";
 import { useDispatch } from "react-redux";
-import { useSelector}  from "react-redux";
 
 const MyCourses = () => {
-  const dispatch             = useDispatch();
+  const dispatch = useDispatch();
   const userFromLocalStorage = JSON.parse(localStorage.getItem("user"));
-  const courses = useSelector(state => state.courses)
 
   useEffect(() => {
-    dispatch(actions.getCoursesOfUser(userFromLocalStorage.id))
-  }, [dispatch])
+    dispatch(actions.resetPaginated())
+    dispatch(actions.getCoursesOfUser(userFromLocalStorage.id));
+  }, [dispatch, userFromLocalStorage.id])
 
   return (
     <div className={styles["my-courses-main"]}>
-      {/* <Paginated actualPage={"MIS_CURSOS"}/> */}
-      <div className={styles["cards-container"]}>
-        {Array.isArray(courses) && courses.map(el => 
-          <CourseCard
-            key={el.id}
-            id={el.id}
-            image={el.image}
-            title={el.title} />
-        )}
-      </div> 
+      <Paginated /> 
     </div>
   );
 };
