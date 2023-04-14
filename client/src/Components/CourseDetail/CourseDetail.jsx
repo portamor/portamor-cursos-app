b4b252dd6ae4d79c3b990655aa9e12f07f8d224a
Seba <sebastian.pitra10@gmail.com>
2023-04-13 22:31:00 -0300

    feat: head instructor added

--- client/src/Components/CourseDetail/CourseDetail.jsx
+++ client/src/Components/CourseDetail/CourseDetail.jsx
@@ -1,32 +1,36 @@
-import * as actions from "../../Redux/actions";
-import Modal from "../Modal/Modal";
-import FormInscription from "../FormInsciption/FormInscription";
-import { NavLink } from "react-router-dom";
-import React from 'react';
-import RegisterUser from "../RegisterUser/RegisterUser"
-import styles from "./CourseDetail.module.css"
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { useMatch } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-import ModalInscription from "../ModalInscription/ModalInscription";
-import usersImg from "../../images/users-icon.svg"
+import * as actions     from "../../Redux/actions";
+import CourseCard       from "../CourseCard/CourseCard";
+import certificateImg   from "../../images/certificate.png"
+import FormInscription  from "../FormInsciption/FormInscription";
 import InstructorDetail from "../InstructorDetail/InstructorDetail";
-import CourseCard from "../CourseCard/CourseCard";
+import Modal            from "../Modal/Modal";
+import ModalInscription from "../ModalInscription/ModalInscription";
+import { NavLink }      from "react-router-dom";
+import React            from 'react';
+import RegisterUser     from "../RegisterUser/RegisterUser"
+import styles           from "./CourseDetail.module.css"
+import { useDispatch }  from "react-redux";
+import { useEffect }    from 'react';
+import { useMatch }     from "react-router-dom";
+import { useSelector }  from "react-redux";
+import { useState }     from 'react';
+import usersImg         from "../../images/users-icon.svg"
+import * as utils       from "../../utils"
 
 export const CourseDetail = () => {
   // const [showModal, setShowModal] = useState(false);
   // const isLoggedIn = useSelector((state) => state.isLoggedIn);
   // const [showInscriptionModal, setShowInscriptionModal] = useState(false);
   
-
   const dispatch = useDispatch();
-  const match = useMatch('/detalle-curso/:courseId');
+  const match    = useMatch('/detalle-curso/:courseId');
   const courseId = match.params.courseId;
-  const courseDetail = useSelector((state) => state.courseDetail);
-  const courseSections = useSelector((state) => state.courseSections);
-  const instructor = useSelector(state => state.courseInstructor);
+
+  const courseDetail    = useSelector((state) => state.courseDetail);
+  const courseSections  = useSelector((state) => state.courseSections);
+  const instructor      = useSelector((state) => state.courseInstructor);
+  const instructorStars = utils.getStarsRating(instructor.score)
+  
 
   useEffect(() => {
     dispatch(actions.getCourseDetail(courseId));
@@ -49,13 +53,23 @@ export const CourseDetail = () => {
 
       <div className={styles["course-detail-head"]}>
         <div className={styles["course-detail-head-container"]}>
+
+
           <div className={styles["head-instructor-container"]}>
-            <p>Instructor</p>
-            <h2 className={styles["head-instructor-name"]}>{instructor.name}</h2>
+            <div className={styles["instructor-name-container"]}>
+              <img src={instructor.profile_picture} alt="instructor-course-detail" className={styles["instructor-head-picture"]}/>
+              <div>
+                <p>Instructor</p>
+                <h2 className={styles["head-instructor-name"]}>{instructor.name}</h2>
+              </div>
+            </div>
+            <span className={styles["stars-container"]}> {instructorStars} </span>
           </div>
 
+
+
           <div className={styles["course-detail-h1-container"]}>
-            <h1>{courseDetail.title}</h1>
+            <h1 className={styles["course-title"]}>{courseDetail.title}</h1>
           </div>
 
           <div className={styles["course-detail-students-container"]}>
@@ -126,6 +140,16 @@ export const CourseDetail = () => {
                 <li>{material}</li>
               )}
               </ol>
+            </div>
+          </div>
+          
+          <div className={styles["certificate-portamor"]}>
+            <div className={styles["course-detail-h1-container"]}>
+              <h1>Certificado digital</h1>
+            </div>
+            <div className={styles["certificate-portamor-container"]}>
+              <img src={certificateImg} alt="certificate-detail" className={styles["certificate-img"]} />
+              <p>“Al final del curso podrás descargar un certificado virtual que acredite que has lo has finalizado con éxito”</p>
             </div>
           </div>
 
