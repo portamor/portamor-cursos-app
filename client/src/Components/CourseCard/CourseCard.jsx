fc07bfd24f4eefbcc513cac74c7d33b791f1f58e
Seba <sebastian.pitra10@gmail.com>
2023-04-15 11:04:47 -0300

    fix(courseCard): it was redesign

--- client/src/Components/CourseCard/CourseCard.jsx
+++ client/src/Components/CourseCard/CourseCard.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Styles from "./CourseCard.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import * as actions from "../../Redux/actions";
 
 const CourseCard = ({ id, title, image }) => {
+  const dispatch = useDispatch();
+
+  const courseUsers = useSelector((state) => state.courseUsers);
+
+  useEffect(() => {
+    // dispatch(actions.getUsersByCourseId(id));
+  }, [courseUsers, dispatch, id])
+
   return (
     <Link to={`/detalle-curso/${id}`} className={Styles["button-link"]}>
       <div className={Styles["course-card"]}>
         <img src={image} alt="course-card" />
         <h3>{title}</h3>
+        <p>{courseUsers.length} Estudiantes</p>
         <button className={Styles["card-button"]}>Ver Más</button>
       </div>
     </Link>
