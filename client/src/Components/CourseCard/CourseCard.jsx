a6755275360fdb965aea17178b6dab068bb3d77f
Lizhana Campos <111819140+Lizhana@users.noreply.github.com>
2023-04-17 00:50:34 -0400

    Se crearon las opciones para Editar curso, eliminar curso

--- client/src/Components/CourseCard/CourseCard.jsx
+++ client/src/Components/CourseCard/CourseCard.jsx
@@ -5,15 +5,42 @@ import Styles          from "./CourseCard.module.css";
 import { useDispatch } from "react-redux";
 import { useEffect }   from "react";
 import { useSelector } from "react-redux";
+import Swal            from "sweetalert2";
 
 const CourseCard = ({ id, title, image }) => {
   const dispatch = useDispatch();
-
-  const courseDetail = useSelector((state) => state.courseDetail);
+  const user = useSelector((state) => state.user);
+  const isAdmin = user?.admin;
+  const courseDetail = useSelector((state) => state?.courseDetail);
 
   useEffect(() => {
     dispatch(actions.getCourseDetail(id));
   }, [dispatch, id])
+
+  const hadleRemove = (e) => {
+    e.preventDefault()
+    Swal.fire({
+      title: '¿Estás seguro de que deseas borrar este curso?',
+      text: 'Esta acción no se puede deshacer',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, borrar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(actions.deleteCourse(id));
+        Swal.fire(
+          '¡Borrado!',
+          'El curso ha sido borrado.',
+          'success'
+        ).then(() => {
+          window.location.reload()
+        });
+      }
+    });
+  };
 
   return (
     <div className={Styles["course-card-main"]}>
@@ -27,6 +54,14 @@ const CourseCard = ({ id, title, image }) => {
           <Link to={`/detalle-curso/${id}`} className={Styles["card-button"]}>
             Ver Más
           </Link>
+          <br />
+          { isAdmin && ( <Link to={`/dashboard`} className={Styles["card-button"]} state={id} >  Editar   
+        </Link> 
+        )}
+        <br />
+          { isAdmin && ( <button className={Styles["card-button"]} onClick={hadleRemove} >  Eliminar curso   
+        </button> 
+        )}
         </div>
     </div>
   );
