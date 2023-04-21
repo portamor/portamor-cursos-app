159d03ebff9a79c6b062685a62cdbe8de114f429
Seba <sebastian.pitra10@gmail.com>
2023-04-21 14:12:50 -0300

    fix(coursecard): info pass through params

--- client/src/Components/CourseCard/CourseCard.jsx
+++ client/src/Components/CourseCard/CourseCard.jsx
@@ -1,23 +1,17 @@
 import * as actions    from "../../Redux/actions";
 import { Link }        from "react-router-dom";
+import { Pen }         from "react-bootstrap-icons"
 import React           from "react";
 import Styles          from "./CourseCard.module.css";
 import { useDispatch } from "react-redux";
-import { useEffect }   from "react";
 import { useSelector } from "react-redux";
 import Swal            from "sweetalert2";
-import { Trash } from "react-bootstrap-icons"
-import { Pen } from "react-bootstrap-icons"
+import { Trash }       from "react-bootstrap-icons"
 
-const CourseCard = ({ id, title, image }) => {
+const CourseCard = ({ id, title, image, duration, level }) => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
   const isAdmin = user?.admin;
-  const courseDetail = useSelector((state) => state?.courseDetail);
-
-  useEffect(() => {
-    dispatch(actions.getCourseDetail(id));
-  }, [dispatch, id])
 
   const hadleRemove = (e) => {
     e.preventDefault()
@@ -59,8 +53,8 @@ const CourseCard = ({ id, title, image }) => {
         }
         <h3>{title}</h3>
         <div className={Styles["course-details-container"]}>
-          <p>{courseDetail.duration}</p>
-          <p>{courseDetail.level}</p>
+          <p>{duration}</p>
+          <p>{level}</p>
         </div>
         <Link to={`/detalle-curso/${id}`} className={Styles["card-button"]}>
           Ver Más
