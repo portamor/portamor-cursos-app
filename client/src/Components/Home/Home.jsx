a4573a73d072d71611cdb802325dfaacc38dc906
Gaby <gaby.es.sd@gmail.com>
2023-09-22 20:37:39 -0500

    Refactor: Update useEffect hook to dispatch getcourses action according to the courses genre value.

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,16 +1,29 @@
-import * as actions    from "../../Redux/actions"
 import Paginated from "../Paginated/Paginated";
-import React     from "react";
+import { useDispatch, useSelector } from "react-redux";
+import React, { useState, useEffect}     from "react";
+import { getCourses, getCoursesByGenre }  from "../../Redux/actions";
 import Styles    from "./Home.module.css";
-import { useEffect }   from "react";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
 import NavFilter from "../NavFilter/NavFilter";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const [coursesGenre, setCoursesGenre] = useState('');
+  const pageSize    = useSelector((state) => state.pageSize);
+
+  useEffect(() => {
+    if (coursesGenre === '')
+      dispatch(getCourses(1, pageSize));
+    else
+      dispatch(getCoursesByGenre(coursesGenre));
+  }, [dispatch, coursesGenre, pageSize]);
+
+  const handleCoursesGenre = (genre) => {
+    setCoursesGenre(genre);
+  };
+
   return (
     <div className={Styles["home-container"]}>
-      <NavFilter actualPage={"HOME"}/>
+      <NavFilter handleCoursesGenre={handleCoursesGenre}/>
       <Paginated />
     </div>
   );
