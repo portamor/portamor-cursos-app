720353dcee8502392d610e2097f0f16d59363d51
Gaby <gaby.es.sd@gmail.com>
2023-10-04 21:20:07 -0500

    Refactor: Resource viewer.

--- client/src/Components/ClassDetail/ClassDetail.jsx
+++ client/src/Components/ClassDetail/ClassDetail.jsx
@@ -1,17 +1,12 @@
-import * as actions    from "../../Redux/actions";
-import certifiedImg    from "../../images/certified-portamor.svg"
-import * as constants  from "../../constants/classDetailConstants";
-import CustomButton    from "../CustomButton/CustomButton";
-import React           from "react";
-import ReactPlayer     from 'react-player';
-import SelectedData    from "../SelectedData/SelectedData";
-import styles          from "./ClassDetail.module.css";
-import { useDispatch } from "react-redux";
-import { useEffect }   from "react";
-import { useMatch }    from "react-router-dom";
-import { useSelector } from "react-redux";
-import { useState }    from "react";
-import * as utils      from "../../utils";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useMatch } from "react-router-dom";
+import ReactPlayer from 'react-player';
+import CustomButton from "../CustomButton/CustomButton";
+import SelectedData from "../SelectedData/SelectedData";
+import * as actions from "../../Redux/actions";
+import * as constants from "../../constants/classDetailConstants";
+import styles from "./ClassDetail.module.css";
 
 const ClassDetail = (props) => {
   const dispatch    = useDispatch();
@@ -32,7 +27,6 @@ const ClassDetail = (props) => {
   const courseDetail = useSelector((state) => state.courseDetail);
   const videosCourse = useSelector((state) => state?.videosOfCourse.flat());
   const videosState  = useSelector((state) => state?.videoStateCourse.flat());
-  const courseRating = utils.getStarsRating(courseDetail.rating);
 
   const [firstSelectedButton, setFirstSelectedButton]   = useState(constants.VER_TEMARIO);
   const [secondSelectedButton, setSecondSelectedButton] = useState(constants.PREGUNTAS_FRECUENTES);
@@ -56,7 +50,6 @@ const ClassDetail = (props) => {
       }
     }, [videosState.length,  videosCourse.length ]);
 
-
   const handleEnd = async () => {
     dispatch(actions.createVideoState(payload))
     if (videosState.length >= videosCourse.length-1 ) {
@@ -66,93 +59,125 @@ const ClassDetail = (props) => {
 
   return (
     <div className={styles["class-detail-main"]}>
-      <div className={styles["video-description-container"]}>
-        <div>
-          <h1 className={styles["course-title"]}>
-            Curso de {" "}
-            <span className={styles["course-title-color"]}>{courseDetail.title}</span>
-          </h1>
+      <div className={styles["class-container"]}>
+        <div className={styles["resource-viewer-container"]}>
           <div className={styles["react-player"]}>
-            <ReactPlayer 
-            url={videoDetail.videoLink} 
-            width='100%'
-            height='100%'
-            onEnded={handleEnd}/>
-          </div>
-        </div>
-        
-        <div className={styles["description-container"]}>
-          <h2 className={styles["description-title"]}>Descripcion del curso</h2>
-          <p className={styles["description-text"]}>{ courseDetail.description }</p>
-          <span className={styles["stars-container"]}>
-            Valoracion del curso: { courseRating }
-          </span>
-          <div className={styles["certified-container"]}>
-            <span>Incluye certificado:</span>
-            <strong>Si</strong>
-            <img src={certifiedImg} alt="certified" />
-          </div>
-          <div className={styles["buttons-container"]}>
-            <CustomButton 
-            primary={firstSelectedButton === constants.VER_TEMARIO ? true : false}  
-            content={constants.VER_TEMARIO}
-            onClick={() => handleFirstSelectData(constants.VER_TEMARIO)} />
-            <CustomButton 
-            primary={firstSelectedButton === constants.SOBRE_EL_INSTRUCTOR ? true : false}  
-            content={constants.SOBRE_EL_INSTRUCTOR}
-            onClick={() => handleFirstSelectData(constants.SOBRE_EL_INSTRUCTOR)} />
-          </div>
-        </div>
-      </div>
-
-      <div className={styles["secciones-temario-container"]}>
-        <div className={styles["buttons-and-cards-container"]}>
-          <div className={styles["buttons-sections-container"]}>
-            <CustomButton 
-            primary={secondSelectedButton === constants.COMENTARIOS ? true : false}  
-            content={constants.COMENTARIOS}
-            onClick={() => handleSecondSelectData(constants.COMENTARIOS)} />
-            <CustomButton 
-            primary={secondSelectedButton === constants.MATERIALES ? true : false}
-            content={constants.MATERIALES}
-            onClick={() => handleSecondSelectData(constants.MATERIALES)} />
-            <CustomButton 
-            primary={secondSelectedButton === constants.PARTICIPANTES ? true : false}
-            content={constants.PARTICIPANTES} 
-            onClick={() => handleSecondSelectData(constants.PARTICIPANTES)} />
-            <CustomButton 
-            primary={secondSelectedButton === constants.PREGUNTAS_FRECUENTES ? true : false}
-            content={constants.PREGUNTAS_FRECUENTES}
-            onClick={() => handleSecondSelectData(constants.PREGUNTAS_FRECUENTES)} />
-
-          { allVideosWatched &&  <CustomButton 
-            primary={secondSelectedButton === constants.DESCARGA_CERTIFICADO ? true : false}
-            content={constants.DESCARGA_CERTIFICADO}
-            onClick={() => handleSecondSelectData(constants.DESCARGA_CERTIFICADO)} />}
-          </div>
-
-          <div className={styles["cards-container"]}>
             {
-            secondSelectedButton && 
-            <SelectedData 
-              courseId={courseId}
-              courseDetail={courseDetail} 
-              selectedButtonContent={secondSelectedButton} />
+              videoDetail.isVideo ?
+              (
+                <ReactPlayer 
+                  url={videoDetail.videoLink}
+                  width='100%'
+                  height='100%'
+                  className={styles["resource-iframe"]}
+                  onEnded={handleEnd}
+                />
+              )
+              :
+              (
+                <>
+                  <iframe
+                    id="resourceIFrame"
+                    style={{margin: 'auto'}}
+                    className={styles["resource-iframe"]}
+                    title="Resource Iframe"
+                    width="100%"
+                    height="100%"
+                    src={videoDetail.videoLink}
+                  >
+                  </iframe>
+                </>
+              )
+            }
+          </div>
+          {
+            !videoDetail.isVideo && (
+              <div className={styles["buttons-container"]}>
+                <CustomButton
+                  primary={true}  
+                  content={'COMPLETADO'}
+                  type={'button'}
+                  onClick={handleEnd}
+                  disabled={false}
+                />
+              </div>
+            )
+          }
+        </div>
+
+        <div className={styles["course-manage-container"]}>
+          <div className={styles["description-container"]}>
+            <h2 className={styles["description-title"]}>{courseDetail.title}</h2>
+          </div>
+
+          <div className={styles["syllabus-instructor-container"]}>
+            <div className={styles["buttons-container"]}>
+              <CustomButton
+                primary={firstSelectedButton === constants.VER_TEMARIO ? true : false}  
+                content={constants.VER_TEMARIO}
+                onClick={() => handleFirstSelectData(constants.VER_TEMARIO)}
+              />
+              <CustomButton
+                primary={firstSelectedButton === constants.SOBRE_EL_INSTRUCTOR ? true : false}  
+                content={constants.SOBRE_EL_INSTRUCTOR}
+                onClick={() => handleFirstSelectData(constants.SOBRE_EL_INSTRUCTOR)}
+              />
+            </div>
+            {
+              firstSelectedButton && 
+              <SelectedData 
+                courseId={courseId}
+                courseDetail={courseDetail} 
+                selectedButtonContent={firstSelectedButton}
+              />
             }
           </div>
         </div>
-        
-        {
-          firstSelectedButton && 
-          <SelectedData 
-            courseId={courseId}
-            courseDetail={courseDetail} 
-            selectedButtonContent={firstSelectedButton} />
-        }
       </div>
+      <div className={styles["options-container"]}>
+        <div className={styles["buttons-container"]}>
+          <CustomButton 
+            primary={secondSelectedButton === constants.COMENTARIOS ? true : false}  
+            content={constants.COMENTARIOS}
+            onClick={() => handleSecondSelectData(constants.COMENTARIOS)}
+          />
+          <CustomButton 
+            primary={secondSelectedButton === constants.MATERIALES ? true : false}
+            content={constants.MATERIALES}
+            onClick={() => handleSecondSelectData(constants.MATERIALES)}
+          />
+          <CustomButton 
+            primary={secondSelectedButton === constants.PARTICIPANTES ? true : false}
+            content={constants.PARTICIPANTES} 
+            onClick={() => handleSecondSelectData(constants.PARTICIPANTES)}
+          />
+          <CustomButton 
+            primary={secondSelectedButton === constants.PREGUNTAS_FRECUENTES ? true : false}
+            content={constants.PREGUNTAS_FRECUENTES}
+            onClick={() => handleSecondSelectData(constants.PREGUNTAS_FRECUENTES)}
+          />
+          { 
+            allVideosWatched &&
+            <CustomButton 
+              primary={secondSelectedButton === constants.DESCARGA_CERTIFICADO ? true : false}
+              content={constants.DESCARGA_CERTIFICADO}
+              onClick={() => handleSecondSelectData(constants.DESCARGA_CERTIFICADO)}
+            />
+          }
+        </div>
 
+        <div className={styles["cards-container"]}>
+          {
+            secondSelectedButton &&
+            <SelectedData 
+              courseId={courseId}
+              courseDetail={courseDetail} 
+              selectedButtonContent={secondSelectedButton}
+            />
+          }
+        </div>
+      </div>
     </div>
-
   )
 };
 
