a22134044bd633e75a61e00c19772a3e8526a4e2
Gaby <gaby.es.sd@gmail.com>
2023-12-24 11:17:12 -0500

    Add created and updated dates to follow the firebasetoken device change.

--- Backend-P/src/models/PaymentDevice.js
+++ Backend-P/src/models/PaymentDevice.js
@@ -22,7 +22,7 @@ module.exports = sequelize => {
             unique: true
         },
         },
-        { timestamps: false,
+        { timestamps: true,
             paranoid: true, }
     )
 }
